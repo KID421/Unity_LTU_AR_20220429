b5e70b9853aab7df7f325abc547ab507a8b9b3f3
KID <50666207+KID421@users.noreply.github.com>
2022-05-27 16:06:01 +0800

    Update 透過 API 控制物件動畫

--- Assets/Scripts/CharacterAnimatorControl.cs
+++ Assets/Scripts/CharacterAnimatorControl.cs
@@ -2,5 +2,28 @@
 
 public class CharacterAnimatorControl : MonoBehaviour
 {
+    // 控制元件的方式
+    // 1. 將元件定義為欄位 field
+    // 2. 透過 Unity API 成員存取
 
+    public Animator aniMing;
+
+    // UI 按鈕 Button 點擊後執行 C# 內容的設定流程
+    // 1. 設定一個公開的方法 public method
+    // 2. C# 腳本必須放在實體物件上變成元件 Component
+    // 3. 按鈕 Button 的 On Click 指定該物件與公開的方法
+
+    public void SetCharacterWalk()
+    {
+        print("小明走路");
+
+        aniMing.SetBool("開關走路", true);
+    }
+
+    public void SetCharacterWave()
+    {
+        print("小明招手");
+
+        aniMing.SetBool("開關走路", false);
+    }
 }
