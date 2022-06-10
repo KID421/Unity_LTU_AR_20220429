776177d4050786921b6a40296ff23805fa4dedf9
KID <50666207+KID421@users.noreply.github.com>
2022-06-10 15:09:32 +0800

    滑桿粒子控制與 Lean Touch

---
+++ Assets/Scripts/ParticleSliderControl.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class ParticleSliderControl : MonoBehaviour
+{
+    [Header("光點粒子系統")]
+    public ParticleSystem psLightPoint;
+
+    // Single 在 C# 為 float
+
+    public void ControlParticleEmission(float sliderValue)
+    {
+        print("滑桿的值：" + sliderValue);
+
+        // emission 指定為 光點粒子的噴射
+        var emission = psLightPoint.emission;
+        // 控制光點的噴射 為 滑桿的值
+        emission.rateOverTime = sliderValue;
+    }
+}
